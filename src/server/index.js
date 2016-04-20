f6e3dfa132e18c35370f906f0dc448bbd42b4e0e
Jorge Ricaldi <jorge.ricaldi@gmail.com>
2016-04-19 20:19:49 -0500

    change default port

--- src/server/index.js
+++ src/server/index.js
@@ -2,12 +2,14 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+var port = process.env.PORT || "3000"
+
 app.use(express.static(path.resolve("src/client")));
 
 app.get('/', function (req, res) {
   res.sendFile(path.resolve("src/client/index.html"));
 });
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });

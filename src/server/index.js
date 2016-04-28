7963eb867e1ab81e1a9543b09821d5093d3d027f
Jorge Ricaldi <jorge.ricaldi@gmail.com>
2016-04-28 17:08:21 -0500

    add knex module and builded mysql bd temporally

--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,28 @@
 var express = require('express');
-var path = require('path');
 var app = express();
+var path = require('path');
+var bodyParser = require("body-parser");
 
 var port = process.env.PORT || "3000"
+var router = express.Router();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.static(path.resolve("src/client")));
 
-app.get('/', function (req, res) {
+/*** html landing page ***/
+app.get("/", function (req, res) {
   res.sendFile(path.resolve("src/client/index.html"));
 });
+/***/
+
+/*** api rest ***/
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+app.use('/api', router);
+/***/
 
 app.listen(port, function () {
   console.log('Example app listening on port 3000!');

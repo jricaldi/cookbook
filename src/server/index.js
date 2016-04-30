83adb2a8b292c39f6b7597b779665e782d5750ae
Jorge Ricaldi <jorge.ricaldi@gmail.com>
2016-04-29 19:19:17 -0500

    developed first rest api using queries Knex

--- src/server/index.js
+++ src/server/index.js
@@ -16,14 +16,112 @@ app.get("/", function (req, res) {
 });
 /***/
 
-/*** api rest ***/
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+var knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host     : '127.0.0.1',
+    user     : 'root',
+    password : '',
+    database : 'cookbook'
+  }
+});
+
+/*** API rest ***/
+/*** Recipes ***/
+var recipesRest = router.route('/recipes');
+
+recipesRest.get(function(req,res){
+  knex.select().from("recipe")
+  .join("ingredient",
+  {"recipe.id_recipe" : "ingredient.id_recipe"})
+  .then(function(rows){
+    console.log(rows);
+    res.json(JSON.parse(JSON.stringify(rows)));
+  });
+});
+
+recipesRest.post(function(req,res){
+  res.json({ message: 'post recipes' });
+});
+
+/*** Recipes/Category:ID ***/
+var recipesCategoryRest = router.route('/recipes/category/:id');
+
+recipesCategoryRest.get(function(req,res){
+  var id = req.params.id;
+  knex.select().from("recipe").join("ingredient",
+  {"recipe.id_recipe" : "ingredient.id_recipe"})
+  .where({id_category : id})
+  .then(function(rows){
+    console.log(rows);
+    res.json(JSON.parse(JSON.stringify(rows)));
+  });
+});
+
+/*** Recipes:ID ***/
+var recipesIDRest = router.route('/recipes/:id');
+
+recipesIDRest.get(function(req,res){
+  var id = req.params.id;
+  knex.select().from("recipe").join("ingredient",
+  {"recipe.id_recipe" : "ingredient.id_recipe"})
+  .where({id_recipe : id})
+  .then(function(rows){
+    console.log(rows);
+    res.json(JSON.parse(JSON.stringify(rows)));
+  });
+});
+
+recipesIDRest.put(function(req,res){
+  var id = req.params.id;
+  var body = req.body;
+  knex.transaction(function(trx){
+
+    knex("recipe").where({id_recipe : id})
+    .update(body).transacting(trx)
+    then(trx.commit).then(trx.rollback);
+
+  })
+  .then(function(result){
+    console.log(result);
+  })
+  .catch(function(error){
+    console.error(error);
+  });
+});
+
+
+recipesIDRest.delete(function(req,res){
+  var id = req.params.id;
+  knex.transaction(function(trx){
+
+    knex("recipe").where({id_recipe : id}).del()
+    .transacting(trx)
+    then(trx.commit).then(trx.rollback);
+
+  })
+  .then(function(result){
+    console.log(result);
+  })
+  .catch(function(error){
+    console.error(error);
+  });
+});
+
+
+/*** Categories ***/
+var categoriesRest = router.route('/categories')
+
+categoriesRest.get(function(req,res){
+  var id = req.params.id;
+  knex.select().from("category").then(function(rows){
+    res.json(JSON.parse(JSON.stringify(rows)));
+  });
 });
 
 app.use('/api', router);
 /***/
 
 app.listen(port, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port ' + port);
 });
